cea859cf372f1e1cb32496006a5e3b256bea85f2
Adam Keeley <uitake@leeds.ac.uk>
2025-04-10 19:21:00 +0100

    Finally got ModelChoiceField to accept initial values by using custom __init__

--- Prism/forms.py
+++ Prism/forms.py
@@ -1,25 +1,48 @@
 from django import forms
+from .models import Tlkstage, Tblproject
 
 
 class ProjectForm(forms.Form):
     projectnumber= forms.CharField(max_length=5)
     projectname= forms.CharField(max_length=500)
-    stage= forms.ChoiceField()              # Need to get options from model
+    stage= forms.ModelChoiceField(queryset=Tlkstage.objects.values_list("pstagedescription", flat=True
+                                                                        ).filter(validto__isnull=True
+                                                                        ).order_by("stagenumber"
+                                                                        ), to_field_name='stageid')             # Need to get options from model
     classification= forms.ChoiceField()     # Need to get options from model
     projectedstartdate= forms.DateField()
     projectedenddate= forms.DateField()
     startdate= forms.DateField()
     enddate= forms.DateField()
-    pi= forms.ChoiceField()                 # Need to get options from model
-    leadapplicant= forms.ChoiceField()      # Need to get options from model
+    pi_fullname= forms.CharField(max_length=50)
+    leadapplicant_fullname= forms.CharField(max_length=50)
     faculty= forms.ChoiceField()            # Need to get options from model
-    lida= forms.CheckboxInput
-    internship= forms.CheckboxInput
-    dspt= forms.CheckboxInput
-    iso27001= forms.CheckboxInput
-    laser= forms.CheckboxInput
-    irc= forms.CheckboxInput
-    seed= forms.CheckboxInput
+    lida= forms.CheckboxInput()
+    internship= forms.CheckboxInput()
+    dspt= forms.CheckboxInput()
+    iso27001= forms.CheckboxInput()
+    laser= forms.CheckboxInput()
+    irc= forms.CheckboxInput()
+    seed= forms.CheckboxInput()
     validfrom= forms.DateField()
     validto= forms.DateField()
     createdby= forms.CharField(max_length=50)
+
+    '''
+    https://www.webforefront.com/django/formprocessing.html
+    listing 6-11 for the win!
+    '''
+
+    def __init__(self, *args, **kwargs):
+                initial_arguments = kwargs.get('initial', None)
+                updated_initial = {}
+                if initial_arguments:
+                    stage = initial_arguments.get('stage__pstagedescription',None)
+                    if stage:
+                          updated_initial['stage'] = stage
+
+                kwargs.update(initial=updated_initial)
+                super(ProjectForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = Tblproject
--- Prism/views.py
+++ Prism/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.apps import apps
 from django.db.models import Value
 from django.db.models.functions import Concat
-from .models import Tblproject, Tlkstage, Tlkclassification, Tlkfaculty
-from datetime import datetime
+from .models import Tblproject
+from .forms import ProjectForm
 
 
 def projects(request):
@@ -21,40 +21,42 @@ def projects(request):
     return render(request, 'projects.html', {'projects':projects})
 
 def project(request, projectnumber):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            return HttpResponse("/thanks/")
+        else:
+            return render(request, 'project.html', {'form':form})
+    
     if request.method == 'GET':
         project = Tblproject.objects.filter(
-                validto__isnull=True
-                , projectnumber=projectnumber
-            ).values(
-                "pid"
-                , "projectnumber"
-                , "projectname"
-                , "stage__pstagedescription"
-                , "classification__classificationdescription"
-                , "projectedstartdate"
-                , "projectedenddate"
-                , "startdate"
-                , "enddate"
-                , "faculty__facultydescription"
-                , "lida"
-                , "internship"
-                , "dspt"
-                , "iso27001"
-                , "laser"
-                , "irc"
-                , "seed"
-                , "validfrom"
-                , "validto"
-                , "createdby"
-            ).annotate(
-                pi_fullname = Concat('pi__firstname', Value(' '), 'pi__lastname')
-                ,leadapplicant_fullname = Concat('leadapplicant__firstname', Value(' '), 'leadapplicant__lastname')
-            )
-        project_dict = dict(project.first())
-        stages = Tlkstage.objects.all().filter(validto__isnull=True)
-        classifications = Tlkclassification.objects.all().filter(validto__isnull=True)
-        faculties = Tlkfaculty.objects.all().filter(validto__isnull=True)
-        return render(request, 'project.html', {'project':project_dict
-                                                , 'stages':stages
-                                                , 'classifications':classifications
-                                                , 'faculties':faculties})
+            validto__isnull=True
+            , projectnumber=projectnumber
+        ).values(
+            "pid"
+            , "projectnumber"
+            , "projectname"
+            , "stage__pstagedescription"
+            , "classification__classificationdescription"
+            , "projectedstartdate"
+            , "projectedenddate"
+            , "startdate"
+            , "enddate"
+            , "faculty__facultydescription"
+            , "lida"
+            , "internship"
+            , "dspt"
+            , "iso27001"
+            , "laser"
+            , "irc"
+            , "seed"
+            , "validfrom"
+            , "validto"
+            , "createdby"
+        ).annotate(
+            pi_fullname = Concat('pi__firstname', Value(' '), 'pi__lastname')
+            ,leadapplicant_fullname = Concat('leadapplicant__firstname', Value(' '), 'leadapplicant__lastname')
+        ).get()         # get() with no arguments will raise an exception if the queryset doesn't contain exactly one item
+        form = ProjectForm(initial=project)
+        return render(request, 'project.html', {'project':project
+                                                , 'form':form})
