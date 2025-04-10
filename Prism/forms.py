2c147f4a69bcadc4a54e83bc05ed14b95a7ba5e8
TenSeconder <email@email.com>
2025-04-10 22:26:16 +0100

    Formatted form to display data types correctly with initial values prepopulated

--- Prism/forms.py
+++ Prism/forms.py
@@ -1,6 +1,11 @@
 from django import forms
-from .models import Tlkstage, Tblproject
+from .models import Tlkstage, Tblproject, Tlkclassification, Tlkfaculty
 
+class DateInput(forms.DateInput):
+    input_type = "date"
+    def __init__(self, **kwargs):
+        kwargs["format"] = "%Y-%m-%d"
+        super().__init__(**kwargs)
 
 class ProjectForm(forms.Form):
     projectnumber= forms.CharField(max_length=5)
@@ -8,25 +13,28 @@ class ProjectForm(forms.Form):
     stage= forms.ModelChoiceField(queryset=Tlkstage.objects.values_list("pstagedescription", flat=True
                                                                         ).filter(validto__isnull=True
                                                                         ).order_by("stagenumber"
-                                                                        ), to_field_name='stageid')             # Need to get options from model
-    classification= forms.ChoiceField()     # Need to get options from model
+                                                                        ), to_field_name='stageid')
+    classification= forms.ModelChoiceField(queryset=Tlkclassification.objects.values_list("classificationdescription", flat=True
+                                                                        ).filter(validto__isnull=True
+                                                                        ).order_by("classificationdescription"
+                                                                        ), to_field_name='classificationid')
     projectedstartdate= forms.DateField()
     projectedenddate= forms.DateField()
     startdate= forms.DateField()
     enddate= forms.DateField()
     pi_fullname= forms.CharField(max_length=50)
     leadapplicant_fullname= forms.CharField(max_length=50)
-    faculty= forms.ChoiceField()            # Need to get options from model
-    lida= forms.CheckboxInput()
-    internship= forms.CheckboxInput()
-    dspt= forms.CheckboxInput()
-    iso27001= forms.CheckboxInput()
-    laser= forms.CheckboxInput()
-    irc= forms.CheckboxInput()
-    seed= forms.CheckboxInput()
-    validfrom= forms.DateField()
-    validto= forms.DateField()
-    createdby= forms.CharField(max_length=50)
+    faculty= forms.ModelChoiceField(queryset=Tlkfaculty.objects.values_list("facultydescription", flat=True
+                                                                        ).filter(validto__isnull=True
+                                                                        ).order_by("facultydescription"
+                                                                        ), to_field_name='facultyid')
+    lida= forms.BooleanField(label="LIDA")
+    internship= forms.BooleanField(label="DSDP")
+    dspt= forms.BooleanField(label="NHS DSPT")
+    iso27001= forms.BooleanField(label="ISO27001")
+    laser= forms.BooleanField(label="LASER")
+    irc= forms.BooleanField(label="IRC")
+    seed= forms.BooleanField(label="SEED")
 
     '''
     https://www.webforefront.com/django/formprocessing.html
@@ -34,15 +42,25 @@ class ProjectForm(forms.Form):
     '''
 
     def __init__(self, *args, **kwargs):
-                initial_arguments = kwargs.get('initial', None)
-                updated_initial = {}
-                if initial_arguments:
-                    stage = initial_arguments.get('stage__pstagedescription',None)
-                    if stage:
-                          updated_initial['stage'] = stage
+        initial_arguments = kwargs.get('initial', None)
+        updated_initial = initial_arguments
+        if initial_arguments:
+            stage = initial_arguments.get('stage__pstagedescription',None)
+            if stage:
+                    updated_initial['stage'] = stage
+            classification = initial_arguments.get('classification__classificationdescription',None)
+            if classification:
+                    updated_initial['classification'] = classification
+            faculty = initial_arguments.get('faculty__facultydescription',None)
+            if faculty:
+                    updated_initial['faculty'] = faculty
+        kwargs.update(initial=updated_initial)
+        super(ProjectForm, self).__init__(*args, **kwargs)
 
-                kwargs.update(initial=updated_initial)
-                super(ProjectForm, self).__init__(*args, **kwargs)
+        self.fields["projectedstartdate"].widget = DateInput()
+        self.fields["projectedenddate"].widget = DateInput()
+        self.fields["startdate"].widget = DateInput()
+        self.fields["enddate"].widget = DateInput()
 
     class Meta:
         model = Tblproject
