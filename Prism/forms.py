62adfcac6e222da74764b7b6e84e66f49412fdf8
Adam Keeley <uitake@leeds.ac.uk>
2025-09-09 16:23:07 +0100

    Custom date widget in form moved inline

--- Prism/forms.py
+++ Prism/forms.py
@@ -16,10 +16,10 @@ class ProjectForm(forms.Form):
     stage_id= forms.ModelChoiceField(label="Stage", queryset=Tlkstage.objects.filter(validto__isnull=True).order_by("stagenumber"))
     classification_id= forms.ModelChoiceField(label="Classification", queryset=Tlkclassification.objects.filter(validto__isnull=True).order_by("classificationdescription"))
     datrag = forms.IntegerField(label="DAT RAG", widget = forms.HiddenInput(), required=False)
-    projectedstartdate= forms.DateField(label="Projected Start Date")
-    projectedenddate= forms.DateField(label="Projected End Date")
-    startdate= forms.DateField(label="Start Date", required=False)
-    enddate= forms.DateField(label="End Date", required=False)
+    projectedstartdate= forms.DateField(label="Projected Start Date", widget = DateInput())
+    projectedenddate= forms.DateField(label="Projected End Date", widget = DateInput())
+    startdate= forms.DateField(label="Start Date", widget = DateInput(), required=False)
+    enddate= forms.DateField(label="End Date", widget = DateInput(), required=False)
     pi = forms.ModelChoiceField(label="PI", queryset=Tbluser.objects.filter(validto__isnull=True).order_by("firstname", "lastname"), to_field_name="usernumber")
     leadapplicant= forms.ModelChoiceField(label="Lead Applicant", queryset=Tbluser.objects.filter(validto__isnull=True).order_by("firstname", "lastname"), to_field_name="usernumber")
     faculty_id= forms.ModelChoiceField(label="Faculty", queryset=Tlkfaculty.objects.filter(validto__isnull=True).order_by("facultydescription"))
@@ -33,19 +33,6 @@ class ProjectForm(forms.Form):
     validfrom=  forms.DateTimeField(widget = forms.HiddenInput(), required=False) 
     validto= forms.DateTimeField(widget = forms.HiddenInput(), required=False)
     createdby= forms.CharField(widget = forms.HiddenInput(), required=False, max_length=50)
-
-    '''
-    https://www.webforefront.com/django/formprocessing.html
-    listing 6-11 for the win!
-    '''
-
-    def __init__(self, *args, **kwargs):
-        super(ProjectForm, self).__init__(*args, **kwargs)
-
-        self.fields["projectedstartdate"].widget = DateInput()
-        self.fields["projectedenddate"].widget = DateInput()
-        self.fields["startdate"].widget = DateInput()
-        self.fields["enddate"].widget = DateInput()
 
     class Meta:
         model = Tblproject
